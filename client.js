b24c8365d7b5381ed9e7f448897e97d8a15c33fe
Anish Kumar Saha <anishksaha1997@gmail.com>
2019-01-25 01:21:43 +0530

    Update client.js
    initial commit

--- client.js
+++ client.js
@@ -1,18 +1,18 @@
 
-
 // client-side
 
 var request = new XMLHttpRequest();
 
 function call()
 {
-    request.open('GET', 'http://192.168.43.249:3000');
+    request.open('GET', 'http://192.168.43.249:3000/inputs');
     request.onload = function () 
     {
-  
-        var data = JSON.parse(this.responseText);
-        $('#thedata').text(data);
+    	    var data = JSON.parse(this.responseText);
+    		console.log(data);
+            $('#thedata').text(data);
     }
     request.send();
-    setInterval(call,3000);
 }
+
+setInterval(call,3000);
